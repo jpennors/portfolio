66e92ff1aeda28ec2cec2dc53aacc3ee7806a482
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2020-03-30 22:32:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+
+import Home from './pages/Home';
+
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
+class App extends React.Component {
+	render() {
+		return (
+			<BrowserRouter basename={PUBLIC_URL}>
+				<Switch>
+					<Route path="/" exact component={Home}/>
+				</Switch>
+			</BrowserRouter>
+		)
+	}
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ajaxGet } from '../utils/Ajax';
+import { asset_url } from '../utils/Config';
+
+
+class Home extends React.Component {
+
+	constructor(props) {
+        super(props);
+	}
+
+    render(){
+        
+
+		return (
+            
+            <div>
+                Hello
+            </div>         
+		);
+	}
+}
+
+
+export default (Home)
+
